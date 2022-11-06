be84ad1479b1b7ce23025d1d3f23db2ca3acb00f
Isaac Gonzalez <84966379+IsaacAGonzalez@users.noreply.github.com>
2022-11-06 10:36:01 -0800

    Iag yelp api (#5)
    * linked scripts, bootstrap, css
    
    * nav bar and carousel
    
    * carousel editing
    
    * added Louis sidebar
    
    * changed carousel height
    
    * added updated carousel and put sidebar/modal outside of carousel parent
    
    * HTML content added
    
    * Bootstrap class change
    
    * Update Final HTML Skeleton Draft, Code Clean up, Logo in Assets
    
    * Search Area Centered HTML
    
    * made carousel image larger
    
    * changed carousel size
    
    * tags and initial js vars
    
    * script updates
    
    * added api fetch request
    
    * commented out incomplete javascript to avoid any breaks on main
    
    Co-authored-by: cmarielorber <109984761+cmarielorber@users.noreply.github.com>
    Co-authored-by: Louis <louyoon89@gmail.com>

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,53 @@
-$(window).on('load', function() {
-    $('#modal').modal('show');
-  });
+// const sidebar = document.querySelector('#offcanvasNavbar');
+// const localBtn = document.querySelector('#searchBrewBtn');
+// const resultsTable = document.querySelector('#tableBrew');
+
+// function granted(usrPos) {
+//   const position = usrPos.coords;
+//   const usrLatitude = position.latitude;
+//   const usrLongitude = position.longitude;
+//   console.log('access granted');
+//   return [usrLatitude, usrLongitude]
+// }
+
+// function denied() {
+//   console.log('access denied');
+//   return false
+// }
+
+// localBtn.addEventListener('click', function (event) {
+//   event.preventDefault();
+//   console.log('Clicked');
+//   let beerPosition = navigator.geolocation.getCurrentPosition(granted, denied);
+
+//   console.log(beerPosition);
+//   if (beerPosition === false) {
+//     console.log("Can't find beer")
+//     return
+//   } else {
+//     console.log('Second Click');
+//   }
+
+
+// });
+
+
+// var myHeaders = new Headers();
+// myHeaders.append("Authorization", "Bearer d-Zk9Nv2t5lmfuhdEBns4h0UgeAFRUV3-eubcRfjSaPKo5aFhZSi_8qfL7xtTsYghIBiwSmEvaA-yZ0L83ac-wgfOQST-XQqCJ0D7QCKPosrrFnLQu0rL0iK9TVjY3Yx");
+
+// var requestOptions = {
+//   method: 'GET',
+//   headers: myHeaders,
+//   redirect: 'follow'
+// };
+
+// fetch("https://api.yelp.com/v3/businesses/search?categories=breweries&location=san diego", requestOptions)
+//   .then(response => response.text())
+//   .then(result => console.log(result))
+//   .catch(error => console.log('error', error));
+
+
+
+$(window).on('load', function () {
+  $('#modal').modal('show');
+});
